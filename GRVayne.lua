e1e211d2e87f167a4f9074f6a648cbe6e63cc1ae
Jo7j <Geo7ge@live.co.uk>
2014-05-26 15:51:21 +0100

    Several Updates
    Smart Q
    Free user support
    Selector lib integration

--- GRVayne.lua
+++ GRVayne.lua
@@ -1,21 +1,26 @@
 --[[ 
 	GameRAT: Vayne
-		rev. 14522
+		rev. 14524
 	
 	What needs to be done?
-	-Hero priority needs to be improved. The current hero priority algorithm does not take into account enemy damage capabilities and invulnerabilities.
 	-Insert minions into table upon loading script.
 	-Auto-condemn gap closers.
+	-Smart Q [WIP]
 	-Issue with calcDamage function, this appears to have something to do with armor penetration/ghostblade
 	-Stop attacking objects if mousePos and myHero are near tumbleExploit
 	-Add more menu options. More customization.
 	-Avoid tumbling in bad situations
-	-Avoid tumbling after enemy is dead
+	-Cancel auto condemn if not going to be stunned [Done but not confirmed]
 ]]--
+
+if VIP_USER and FileExist(LIB_PATH .. "/Selector.lua") then
+	require "Selector"
+	local USE_SELECTOR = true
+end
 if myHero.charName ~= "Vayne" then return end
 
 --[ UPDATE ]--
-local local_version = 14522
+local local_version = 14524
 local server_version = tonumber(GetWebResult("raw.github.com", "/Jo7j/BoL/master/version/GRVayne.version"))
 if server_version > local_version then
 	PrintChat("Script is outdated. Updating to version: " .. server_version .. "...")
@@ -35,6 +40,9 @@ local tumbleExploit = {
 	{startPos=Vector(11590.95, 51.98, 4656.26), endPos=Vector(11334.74, -62.83, 4517.47)},  -- bot
 	{startPos=Vector(6639, 56.01, 8653), endPos=Vector(6364.96, -65.07, 8508.06)} -- mid
 }
+local skillshotsToAvoid = {
+	["CaitlynPiltoverPeacemaker"] = {projSpeed = 2200, radius = 90}, 
+}
 --[ End of Data ]--
 local enemyHeroes = GetEnemyHeroes()
 local projSpeed = champTable()[myHero.charName].projSpeed
@@ -44,7 +52,8 @@ local lastAttack = 0
 local windUpTime = 0
 local attackCooldown = 0
 local targetArray = {}
-local wayPointManager = WayPointManager()
+local tp = VIP_USER and TargetPredictionVIP(715, 1200, 0.25) or TargetPrediction(715, 1200, 0.25)
+local tp = VIP_USER and TargetPredictionVIP(715, 1200, 0.25) or TargetPrediction(715, 1200, 0.25)
 
 --[ Functions ]--
 function getTick()
@@ -89,7 +98,7 @@ function calcDamage(src, dst, add)
 		if myHero:GetSpellData(_Q).level > 0 and myHero:CanUseSpell(_Q) == SUPRESSED then
 			totalDamage = totalDamage+(myHero.totalDamage/100)*(myHero:GetSpellData(_Q).level*5+25)
 		end
-		if VayneWParticle and VayneWParticle.valid and GetDistance(VayneWParticle, dst) < 10 then
+		if VayneWRing2 and VayneWRing2.valid and GetDistance(VayneWRing2, dst) < 10 then
 			totalDamage = totalDamage+((dst.maxHealth/100)*(myHero:GetSpellData(_W).level+3))+(10+(10*myHero:GetSpellData(_W).level))
 		end
 	end
@@ -110,16 +119,8 @@ function calcDamage(src, dst, add)
 	return damageMultiplier * totalDamage
 end
 
-function canCondemn(pos, enemy, timeDelay)
-	local predictPos = enemy
-	if VIP_USER then
-		local result, fTime = wayPointManager:GetSimulatedWayPoints(enemy)
-		if #result ~= 1 then
-			local wayPoint = Vector(result[2].x, enemy.y, result[2].y)
-			local delay = GetDistance(myHero, enemy)/1200
-			local predictPos = enemy+(wayPoint-enemy):normalized()*(enemy.ms*delay+(timeDelay or 0))
-		end
-	end
+function canCondemn(pos, enemy)
+	local predictPos = tp:GetPrediction(enemy) or enemy
 	for k=1, 390, 65 do -- 450 is max push distance
 		if enemy.x > 0 and enemy.z > 0 then
 			local pushPos = predictPos+(Vector(predictPos)-pos):normalized()*k
@@ -130,6 +131,7 @@ function canCondemn(pos, enemy, timeDelay)
 	end
 	return false
 end
+
 function getPredictedHealth(minion, delay)
 	local totalDamage = 0
 	if minion.incomingDamage then
@@ -142,6 +144,75 @@ function getPredictedHealth(minion, delay)
 	return minion.health-totalDamage
 end
 
+function isFacing(source, target, lineLength)
+	local sourceVector = Vector(source.visionPos.x, source.visionPos.z)
+	local sourcePos = Vector(source.x, source.z)
+	sourceVector = (sourceVector-sourcePos):normalized()
+	sourceVector = sourcePos + (sourceVector*(GetDistance(target, source)))
+	return GetDistanceSqr(target, {x = sourceVector.x, z = sourceVector.y}) <= (lineLength and lineLength^2 or 90000)
+end
+
+function smartQ(enemy)
+	--Later, when I have time, this function will return false if certain enemy spells are not on cooldown such as RocketGrab
+	local distance = GetDistance(myHero, enemy)
+	
+	if enemy and enemy.type == "obj_AI_Hero" and distance < getTrueRange()+300 and enemy.health > calcDamage(myHero, enemy) then
+		local tumblePos = mousePos
+		if GRVayne.smartQ then
+			if distance < getTrueRange() and isFacing(enemy, myHero) and distance < GetDistance(mousePos, enemy) then -- kiting
+				tumblePos = myHero+(Vector(enemy)-myHero):normalized()*-300
+			elseif GetDistance(mousePos, enemy) < 200 and isFacing(myHero, enemy) and not isFacing(enemy, myHero) then -- chasing
+				tumblePos = myHero+(Vector(enemy)-myHero):normalized()*300
+			else
+				local angle = math.acos( (distance^2 + 300^2 - getTrueRange()^2) / (2 * distance * 300) )
+				--PrintChat("deg: " .. math.deg(angle))
+				tumblePos = Vector(myHero.x+(math.cos(angle)*300), myHero.y, myHero.z+(math.sin(angle)*300))
+			end
+
+		end
+		CastSpell(_Q, tumblePos.x, tumblePos.z)
+	end
+	return false
+end
+
+function findTarget(method, range)
+	local result = nil
+	if method == 0 then --LOWEST
+		local LOWEST = math.huge
+		for _, enemy in pairs(enemyHeroes) do
+			if GetDistance(myHero, enemy) < range then
+				local difficulty = (100/(100+enemy.armor)*enemy.health)/calcDamage(myHero, enemy)
+				if difficulty < LOWEST then
+					LOWEST = difficulty
+					result = enemy
+				end
+			end
+		end
+	elseif method == 1 then --MOST_DANGEROUS
+		local MOST_DANGEROUS = math.huge
+		for _, enemy in pairs(enemyHeroes) do
+			if GetDistance(myHero, enemy) < range then
+				local capability = (enemy.totalDamage*enemy.attackSpeed)*enemy.armorPenPercent
+				if enemy.ap > enemy.totalDamage then
+					local manaCost = 0
+					local spells = {_Q, _W, _E, _R}
+					for k, spell in pairs(spells) do
+						if enemy:CanUseSpell(spell) == READY then
+							manaCost = manaCost+enemy:GetSpellData(spell).mana
+						end
+					end
+					local capability = (enemy.ap*(enemy.mana/manaCost))*enemy.magicPenPercent
+				end
+				if capability > MOST_DANGEROUS then
+					MOST_DANGEROUS = capability
+					result = enemy
+				end
+			end
+		end
+	end
+	return result
+end
+
 --[ Callbacks ]--
 function OnLoad()
 	GRVayne = scriptConfig("GameRAT: Vayne", "GRVayne")
@@ -151,7 +222,12 @@ function OnLoad()
 	GRVayne:addParam("drawCondemn", "Draw Condemn", SCRIPT_PARAM_ONOFF, false)
 	GRVayne:addParam("priority", "Prioritize minions over harass", SCRIPT_PARAM_ONOFF, true)
 	GRVayne:addParam("tumbleExploit", "Tumble Over Walls", SCRIPT_PARAM_ONOFF, true)
+	GRVayne:addParam("smartQ", "Smart Q", SCRIPT_PARAM_ONOFF, false)
 	GRVayne:addParam("debug", "Debug", SCRIPT_PARAM_ONOFF, false)
+	
+	if USE_SELECTOR then
+		Selector.Instance()
+	end
 	
 	PrintChat("GameRAT: Vayne")
 end
@@ -164,8 +240,11 @@ function OnProcessSpell(object, spellProc)
 			windUpTime = spellProc.windUpTime*1000
 			attackCooldown = spellProc.animationTime*1000
 			
-			if spellProc.target.type == "obj_AI_Hero" then DelayAction(function() CastSpell(_Q, mousePos.x, mousePos.z) end, spellProc.windUpTime) end
-		elseif spellName:find("tumble") or spellName:find("condemn") then
+			DelayAction(function()
+				smartQ(spellProc.target)
+			end, spellProc.windUpTime)
+			
+		elseif spellName:find("tumble") then
 			attackCooldown = 0
 			lastAttack = 0
 		end
@@ -188,10 +267,11 @@ function OnProcessSpell(object, spellProc)
 end
 
 function OnCreateObj(object)
-	if object ~= nil and object.type == "obj_AI_Minion" then
+	if object ~= nil and object.type == "obj_AI_Minion" and object.name:find("Minion") then
 		for i, minion in pairs(targetArray) do
 			if minion.health > object.health then
 				table.insert(targetArray, i-1, object)
+				--PrintChat(object.name)
 				return
 			end
 		end
@@ -199,7 +279,7 @@ function OnCreateObj(object)
 	end
 	
 	if GetDistance(myHero, object) < 1000 and object.name:lower():find("vayne_w_ring2.troy") then
-		VayneWParticle = object
+		VayneWRing2 = object
 	end
 end
 
@@ -209,18 +289,12 @@ function autoCombo_OnTick()
 	
 	if not QREADY and not EREADY then return false end
 	
-	for _, enemy in pairs(enemyHeroes) do
-		if ValidTarget(enemy) then
+	local enemy = USE_SELECTOR and Selector.GetTarget(MOST_DANGEROUS_ADVANCED, 715) or findTarget(1, 715)
+	if enemy then
 			if EREADY and myHero.mana > 90 then -- Auto Condemn
 				if GetDistance(myHero, enemy) < 715 and canCondemn(myHero, enemy) then
 					CastSpell(_E, enemy)
 					return true
-				elseif QREADY and myHero.mana > 120 then -- Tumble then condemn
-					local tumblePos = myHero+(Vector(enemy)-myHero):normalized()*300
-					if GetDistance(tumblePos, enemy) < 715 and canCondemn(tumblePos, enemy) then
-						CastSpell(_Q, tumblePos.x, tumblePos.z)
-						return true
-					end
 				end
 			end
 			
@@ -232,36 +306,17 @@ function autoCombo_OnTick()
 					return true
 				end
 			end
-		end
 	end
-	return false
 end
 
 function autoHarass_OnTick()
-	local targetHero = {difficulty=math.huge}
-	for _, enemy in ipairs(enemyHeroes) do
-		local difficulty = (100/(100+enemy.armor)*enemy.health)/calcDamage(myHero, enemy)
-		if ValidTarget(enemy, getTrueRange()) then
-			if calcDamage(myHero, enemy) >= enemy.health then
-				targetHero = enemy
-				targetHero.difficulty = difficulty
-				break
-			elseif VayneWParticle and VayneWParticle.valid and GetDistance(VayneWParticle, enemy) < 10 then
-				targetHero = enemy
-				targetHero.difficulty = difficulty
-				break
-			elseif difficulty < targetHero.difficulty then
-				targetHero = enemy
-				targetHero.difficulty = difficulty
-			end
-		end
-	end
-	if targetHero.difficulty ~= math.huge then
+	local enemy = USE_SELECTOR and Selector.GetTarget(LOWEST, getTrueRange()) or findTarget(0, getTrueRange())
+	if enemy then
 		lastAttack = getTick()+getLatency()
 		if VIP_USER then
-			Packet('S_MOVE', {type=3, targetNetworkId=targetHero.networkID}):send()
+			Packet('S_MOVE', {type=3, targetNetworkId=enemy.networkID}):send()
 		else
-			myHero:Attack(targetHero)
+			myHero:Attack(enemy)
 		end
 		return true
 	end
@@ -273,14 +328,7 @@ function lastHit_OnTick()
 		local delay = getTick()+windUpTime+(GetDistance(myHero, minion)/projSpeed)+getLatency()
 		if ValidTarget(minion, getTrueRange()) and myHero.team ~= minion.team then
 			local totalDamage = 0
-			if minion.incomingDamage ~= nil then
-				for _, cache in pairs(minion.incomingDamage) do
-					if delay > cache.fin then
-						totalDamage = totalDamage+cache.dmg
-					end
-				end
-			end
-			if totalDamage < minion.health and calcDamage(myHero, minion)+totalDamage-2 > minion.health then
+			if calcDamage(myHero, minion) > getPredictedHealth(minion, delay) then
 				lastAttack = getTick()+getLatency()
 				if VIP_USER then
 					Packet('S_MOVE', {type=3, targetNetworkId=minion.networkID}):send()
@@ -361,9 +409,21 @@ end
 
 function OnDraw()
 	if GRVayne.debug then
-		-- Target Selector Debug
-		for i, enemy in pairs(enemyHeroes) do
-			DrawText(enemy.charName .. ": " .. math.round((100/(100+enemy.armor)*enemy.health)/calcDamage(myHero, enemy), 1), 16, 10, 10+(i*16), ARGB(255, 255, 255, 255))
+		for _, enemy in pairs(enemyHeroes) do
+			local distance = GetDistance(myHero, enemy)
+			if ValidTarget(enemy, getTrueRange()+300) then
+				local tumblePos = myHero+(Vector(enemy)-myHero)
+				if distance < getTrueRange() and isFacing(enemy, myHero) then -- kiting
+					tumblePos = tumblePos:normalized()*-300
+				elseif GetDistance(mousePos, enemy) < 200 and isFacing(myHero, enemy) and not isFacing(enemy, myHero) then -- chasing
+					tumblePos = tumblePos:normalized()*300
+				else
+					local angle = math.acos( (distance^2 + 300^2 - getTrueRange()^2) / (2 * distance * 300) )
+					--PrintChat("deg: " .. math.deg(angle))
+					tumblePos = Vector(myHero.x+(math.cos(angle)*300), myHero.y, myHero.z+(math.sin(angle)*300))
+				end
+				DrawCircle(tumblePos.x, tumblePos.y, tumblePos.z, 50, ARGB(255, 255, 255, 255))
+			end
 		end
 	end
 
@@ -372,6 +432,7 @@ function OnDraw()
 			DrawCircle(exploit.startPos.x, exploit.startPos.y, exploit.startPos.z, 10, ARGB(255,255,255,255))
 			DrawCircle(exploit.endPos.x, exploit.endPos.y, exploit.endPos.z, 10, ARGB(255,255,255,255))
 			DrawLine3D(exploit.startPos.x, exploit.startPos.y, exploit.startPos.z, exploit.endPos.x, exploit.endPos.y, exploit.endPos.z, 1, ARGB(255,255,255,255))
+			if GRVayne.debug then DrawLine3D(exploit.startPos.x, exploit.startPos.y, exploit.startPos.z, exploit.startPos.x, exploit.startPos.y, exploit.startPos.z-GetDistance(exploit.startPos, exploit.endPos), 1, ARGB(255,255,255,255)) end
 		end
 	end
 
@@ -381,26 +442,6 @@ function OnDraw()
 		if ValidTarget(minion, getTrueRange()) and myHero.team ~= minion.team then
 			if calcDamage(myHero, minion) > minion.health then
 				DrawCircle(minion.x, minion.y, minion.z, 100, ARGB(255, 255, 0, 255))
-			end
-		end
-	end
-
-	if GRVayne.drawCondemn then
-		for _, enemy in pairs(enemyHeroes) do
-			if ValidTarget(enemy, 2000) then
-				local predictPos = enemy
-				if VIP_USER then
-					local result, fTime = wayPointManager:GetSimulatedWayPoints(enemy)
-					if #result ~= 1 then
-						local wayPoint = Vector(result[2].x, enemy.y, result[2].y)
-						local delay = GetDistance(myHero, enemy)/1.2
-						local predictPos = enemy+(wayPoint-enemy):normalized()*(enemy.ms*delay)
-					end
-				end
-				if enemy.x > 0 and enemy.z > 0 then
-					local pushPos = predictPos+(Vector(predictPos)-myHero):normalized()*390
-					DrawCircle(pushPos.x, pushPos.y, pushPos.z, 50, ARGB(255, 255, 255, 255))
-				end
 			end
 		end
 	end
