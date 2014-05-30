77d31d8fb88a9564d18132dbd587707c17cac0e7
Jo7j <Geo7ge@live.co.uk>
2014-05-30 15:28:06 +0100

    Update myVision.lua

--- myVision.lua
+++ myVision.lua
@@ -3,6 +3,32 @@ local WARD_RANGE = 1450
 local SPRITE_LOCATION = "myVision\\"
 --[ END OF CONSTANTS ]--
 
+
+PrintChat(SPRITE_PATH .. SPRITE_LOCATION .. "SummonerFlash.png")
+local local_version = 14529
+local server_version = tonumber(GetWebResult("raw.github.com", "/Jo7j/BoL/master/version/myVision.version"))
+if server_version > local_version then
+	PrintChat("Script is outdated. Updating to version: " .. server_version .. "...")
+	DownloadFile("https://raw.github.com/Jo7j/BoL/master/myVision.lua", SCRIPT_PATH .. "myVision.lua", function()
+			PrintChat("Script updated. Please reload (F9).")
+	end)
+end
+
+if not DirectoryExist(SPRITE_PATH .. SPRITE_LOCATION) then
+	CreateDirectory(SPRITE_PATH .. SPRITE_LOCATION)
+end
+
+local sprites = {"SummonerClairvoyance.png", "SummonerBarrier.png", "SummonerBoost.png", "SummonerDot.png", "SummonerExhaust.png", "SummonerFlash.png", "SummonerHaste.png", "SummonerHeal.png", "SummonerMana.png", "SummonerRevive.png", "SummonerSmite.png", "SummonerTeleport.png", "Minimap_Ward_Green_Enemy.png", "Minimap_Ward_Pink_Enemy.png"}
+for _, sprite in pairs(sprites) do
+	if not FileExist(SPRITE_PATH .. SPRITE_LOCATION .. sprite) then
+		DownloadFile("https://raw.github.com/Jo7j/BoL/master/Sprites/" .. sprite, SPRITE_PATH .. SPRITE_LOCATION .. sprite, function()
+			PrintChat(sprite .. " downloaded.")
+		end)
+	end
+end
+
+if server_version > local_version then return end
+
 --[ GLOBALS ]--
 local spellMeta = {
 	{name="SummonerClairvoyance", sprite=createSprite(SPRITE_LOCATION .. "SummonerClairvoyance.png")}, 
@@ -47,27 +73,6 @@ local wayPointManager = WayPointManager()
 --[ END OF GLOBALS ]--
 
 --[ FUNCTIONS ]--
-function update()
-	local local_version = 14530
-	local server_version = tonumber(GetWebResult("raw.github.com", "/Jo7j/BoL/master/version/myVision.version"))
-	if server_version > local_version then
-		PrintChat("Script is outdated. Updating to version: " .. server_version .. "...")
-		DownloadFile("https://raw.github.com/Jo7j/BoL/master/myVision.lua", SCRIPT_PATH .. "myVision.lua", function()
-				PrintChat("Script updated. Please reload (F9).")
-			end)
-	end
-	
-	local sprites = {"SummonerClairvoyance.png", "SummonerBarrier.png", "SummonerBoost.png", "SummonerDot.png", "SummonerExhaust.png", "SummonerFlash.png", "SummonerHaste.png", "SummonerHeal.png", "SummonerMana.png", "SummonerRevive.png", "SummonerSmite.png", "SummonerTeleport.png", "Minimap_Ward_Green_Enemy.png", "Minimap_Ward_Pink_Enemy.png"}
-	
-	for _, sprite in pairs(sprites) do
-		if not FileExist(SPRITE_PATH .. SPRITE_LOCATION .. sprite) then
-			DownloadFile("https://raw.github.com/Jo7j/BoL/master/Sprites/" .. sprite, SPRITE_PATH .. SPRITE_LOCATION .. sprite, function()
-					PrintChat(sprite .. "downloaded.")
-				end)
-		end
-	end
-end
-
 function objectExist(object)
 	for _, obj in pairs(hiddenObjects.objects) do
 		if object.name == obj.name and object.x > obj.x+100 and object.x < obj.x-100 and object.z > obj.z+100 and object.z < obj.z-100 then
