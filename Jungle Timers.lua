1e8e79ff0f00483abe7c88c306076370d8185ec9
Jo7j <Geo7ge@live.co.uk>
2014-07-07 14:32:55 +0100

    Update Jungle Timers.lua

--- Jungle Timers.lua
+++ Jungle Timers.lua
@@ -1,47 +1,71 @@
 --[[
 	Jungle Timers
+	Thanks to TheSaint for helping with the Twisted Treeline map
 	
-	If you think a jungle camp has been cleared but don't have vision of it, 
-	press 'K' on camp (can also be done through minimap).
+	If you think a jungle camp has been cleared but don't have vision of it, press 'K' on camp
 ]]
 
-local HOTKEY = 'K'
+local jungle = GetGame().map.index
 
-local jungle = {
-	{name = "Blue", spawnTime = 115, respawnTime = 5*60, position = {x = 3608, y = 7800}}, -- Bottom Blue
-	{name = "Wolves", spawnTime = 125, respawnTime = 50, position = {x = 3344, y = 6472}}, -- Bottom Wolves
-	{name = "Wraiths", spawnTime = 125, respawnTime = 50, position = {x = 6240, y = 5410}}, -- Bottom Wraiths
-	{name = "Red", spawnTime = 115, respawnTime = 5*60, position = {x = 7425, y = 4215}}, -- Bottom Red
-	{name = "Golems", spawnTime = 125, respawnTime = 50, position = {x = 7886, y = 2688}}, -- Bottom Golems
-	{name = "Dragon", spawnTime = 2*60+30, respawnTime = 6*60, position = {x = 9465, y = 4348}}, -- Dragon
-	{name = "Blue", spawnTime = 115, respawnTime = 5*60, position = {x = 10321, y = 7070}}, -- Top Blue
-	{name = "Wolves", spawnTime = 125, respawnTime = 50, position = {x = 10625, y = 8620}}, -- Top Wolves
-	{name = "Wraiths", spawnTime = 125, respawnTime = 50, position = {x = 7502, y = 9820}}, -- Top Wraiths
-	{name = "Red", spawnTime = 115, respawnTime = 5*60, position = {x = 6429, y = 11085}}, -- Top Red
-	{name = "Golems", spawnTime = 125, respawnTime = 50, position = {x = 5700, y = 12449}}, -- Top Golems
-	{name = "Baron", spawnTime = 15*60, respawnTime = 7*60, position = {x = 4542, y = 10771}}, -- Baron
-	{name = "Wight", spawnTime = 125, respawnTime = 50, position = {x = 1518, y = 8730}}, -- Bottom Wight
-	{name = "Wight", spawnTime = 125, respawnTime = 50, position = {x = 12357, y = 6762}},  -- Top Wight
-}
+PrintChat(""..jungle)
+
+if jungle == 1 then
+	jungle = {
+		{name = "Blue", spawnTime = 115, respawnTime = 5*60, position = {x = 3608, y = 7800}}, -- Bottom Blue
+		{name = "Wolves", spawnTime = 125, respawnTime = 50, position = {x = 3344, y = 6472}}, -- Bottom Wolves
+		{name = "Wraiths", spawnTime = 125, respawnTime = 50, position = {x = 6240, y = 5410}}, -- Bottom Wraiths
+		{name = "Red", spawnTime = 115, respawnTime = 5*60, position = {x = 7425, y = 4215}}, -- Bottom Red
+		{name = "Golems", spawnTime = 125, respawnTime = 50, position = {x = 7886, y = 2688}}, -- Bottom Golems
+		{name = "Dragon", spawnTime = 2*60+30, respawnTime = 6*60, position = {x = 9465, y = 4348}}, -- Dragon
+		{name = "Blue", spawnTime = 115, respawnTime = 5*60, position = {x = 10321, y = 7070}}, -- Top Blue
+		{name = "Wolves", spawnTime = 125, respawnTime = 50, position = {x = 10625, y = 8620}}, -- Top Wolves
+		{name = "Wraiths", spawnTime = 125, respawnTime = 50, position = {x = 7502, y = 9820}}, -- Top Wraiths
+		{name = "Red", spawnTime = 115, respawnTime = 5*60, position = {x = 6429, y = 11085}}, -- Top Red
+		{name = "Golems", spawnTime = 125, respawnTime = 50, position = {x = 5700, y = 12449}}, -- Top Golems
+		{name = "Baron", spawnTime = 15*60, respawnTime = 7*60, position = {x = 4542, y = 10771}}, -- Baron
+		{name = "Wight", spawnTime = 125, respawnTime = 50, position = {x = 1518, y = 8730}}, -- Bottom Wight
+		{name = "Wight", spawnTime = 125, respawnTime = 50, position = {x = 12357, y = 6762}},  -- Top Wight
+	}
+elseif jungle == 10 then
+	jungle = {
+		{name = "Wraiths", spawnTime = 100, respawnTime = 50, position = {x = 4319, y = 6019}}, -- Left Wraiths
+		{name = "Golems", spawnTime = 100, respawnTime = 50, position = {x = 5086, y = 8329}},  -- Left Golems
+		{name = "Wolves", spawnTime = 100, respawnTime = 50, position = {x = 6131, y = 6159}},  -- Left Wolves
+		{name = "Wraiths", spawnTime = 100, respawnTime = 50, position = {x = 10939, y = 6019}}, -- Right Wraiths
+		{name = "Golems", spawnTime = 100, respawnTime = 50, position = {x = 10312, y = 8329}},  -- Right Golems
+		{name = "Wolves", spawnTime = 100, respawnTime = 50, position = {x = 9197, y = 6159}},  -- Right Wolves
+		{name = "Heal", spawnTime = 180, respawnTime = 90, position = {x = 7446, y = 7206}}, -- Heal
+		{name = "Vilemaw", spawnTime = 600, respawnTime = 60*5, position = {x = 7664, y = 10359}}, -- Vilemaw
+	}
+else
+	PrintChat("Jungle Timers: Map not supported")
+end
+
+function OnLoad()
+	Config = scriptConfig("Jungle Timers", "jungleTimers")
+	Config:addParam("hotkey", "Clear camp hotkey" , SCRIPT_PARAM_ONKEYDOWN, false, GetKey('K'))
+	Config:addParam("textSize", "Minimap text size", SCRIPT_PARAM_SLICE, 14, 10, 20, 0)
+	Config:addParam("textColor", "Minimap text color", SCRIPT_PARAM_COLOR, {255, 255, 255, 255})
+end
 
 function OnRecvPacket(p)
 	if p.header == 195 then
 		p.pos = 9
 		local campID = p:Decode4()
-		jungle[campID].spawnTime = GetGameTimer()+jungle[campID].respawnTime
+		jungle[campID].spawnTime = os.clock()+jungle[campID].respawnTime
 	elseif p.header == 233 then
 		p.pos = 21
 		local campID = p:Decode1()
-		if jungle[campID] then
-			jungle[campID].spawnTime = GetGameTimer()-jungle[campID].respawnTime
+		if jungle[campID] ~= nil then
+			jungle[campID].spawnTime = os.clock()-jungle[campID].respawnTime
 		end
 	end
 end
 
-function OnWndMsg(msg, wParam)
-	if msg == KEY_DOWN and wParam == GetKey(HOTKEY) then
+function OnDraw()
+	if Config.hotkey then
 		for i, camp in ipairs(jungle) do
-			if GetDistance(mousePos, camp.position) < 1000 then
+			if GetDistance(mousePos, camp.position) < 500 then
 				
 				local p = CLoLPacket(195)
 				p:Encode4(0)
@@ -50,16 +74,18 @@ function OnWndMsg(msg, wParam)
 				p:Encode1(3)
 				RecvPacket(p)
 				
-				jungle[i].spawnTime = GetGameTimer()+jungle[i].respawnTime
+				jungle[i].spawnTime = os.clock()+jungle[i].respawnTime
 			end
 		end
 	end
-end
-
-function OnDraw()
+	
 	for i, camp in ipairs(jungle) do
-		if GetGameTimer() < camp.spawnTime then
-			DrawText(tostring(math.ceil(camp.spawnTime-GetGameTimer())), 16, GetMinimapX(camp.position.x), GetMinimapY(camp.position.y), ARGB(255, 255, 255, 255))
+		if os.clock() < camp.spawnTime then
+			local t = camp.spawnTime-os.clock()
+			local m = math.floor(t/60)
+			local s = math.ceil(t%60)
+			s = (s<10 and "0"..s) or s
+			DrawText(m..":"..s, Config.textSize, GetMinimapX(camp.position.x), GetMinimapY(camp.position.y), ARGB(Config.textColor[1], Config.textColor[2], Config.textColor[3], Config.textColor[4]))
 		end
 	end
 end
