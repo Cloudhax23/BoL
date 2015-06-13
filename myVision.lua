077b9eed624a025d02587994b2c910aeedf3951f
Jorj <Geo7ge@live.co.uk>
2015-06-13 21:08:11 +0100

    Revision 13
    Overall improvement of performance
    Small visual tweak to ability bars (gradient effect + no sprite ability frame)
    Fixed bug that occurs with Rengar's Q (total cooldown returning zero causing bad calculations)
    Improved HUD position calculation, should be more accurate now
    Added decimal point to numeric cooldowns if lowers than 10

--- myVision.lua
+++ myVision.lua
@@ -9,63 +9,77 @@
 			open-source awareness script
 ]]
 
-local revision = 12
+local REVISION = 13
+
+local allyHeroes, enemyHeroes = GetAllyHeroes(), GetEnemyHeroes()
+local rad, sin, cos = math.rad, math.sin, math.cos
 
 local abilityFrame = FileExist(SPRITE_PATH .. "myVision\\abilityFrame.png") and createSprite("myVision\\abilityFrame.png")
 local summonerSprites = { }
+
+for _, spellName in pairs({"itemsmiteaoe", "s5_summonersmiteduel", "s5_summonersmiteplayerganker", "s5_summonersmitequick", 
+	"snowballfollowupcast", "summonerbarrier", "summonerboost", "summonerclairvoyance", "summonerdot", 
+	"summonerexhaust", "summonerflash", "summonerhaste", "summonerheal", "summonermana", 
+	"summonerodingarrison", "summonerpororecall", "summonerporothrow", "summonerrevive", "summonersmite", 
+	"summonersnowball", "summonerteleport"}) do
+	local location = "myVision\\spells\\" .. spellName .. ".png"
+
+	if FileExist(SPRITE_PATH .. location) then
+		summonerSprites[spellName] = createSprite(location)
+	else
+		Print(location .. " not found.")
+	end
+end
+
+summonerSprites["teleportcancel"] = summonerSprites["summonerteleport"]
+
 local spellData = { }
 
+for i = 1, heroManager.iCount do
+	local hero = heroManager:getHero(i)
+
+	if hero and hero.valid then
+		spellData[hero.index] = {
+			[_Q] = hero:GetSpellData(_Q), 
+			[_W] = hero:GetSpellData(_W), 
+			[_E] = hero:GetSpellData(_E), 
+			[_R] = hero:GetSpellData(_R), 
+			[SUMMONER_1] = hero:GetSpellData(SUMMONER_1), 
+			[SUMMONER_2] = hero:GetSpellData(SUMMONER_2), 
+		}
+	end
+end
+
 function OnLoad()
-	-- Get latest revision
 	local latest = tonumber(GetWebResult("raw.github.com", "/Jo7j/BoL/master/myVision.rev"))
-	if latest > revision then
+
+	if latest > REVISION then
 		Print("A new update is available. Please update using the menu.")
 	end
 
-	Config = scriptConfig("myVision (rev. " .. revision .. ")", "myVision")
+	Config = scriptConfig("myVision (rev. " .. REVISION .. ")", "myVision")
 		Config:addSubMenu("Ally Settings", "ally")
 			Config.ally:addParam("active", "Enabled", SCRIPT_PARAM_ONOFF, false)
-			Config.ally:addParam("self", "Local Hero", SCRIPT_PARAM_ONOFF, false)
 
 		Config:addSubMenu("Enemy Settings", "enemy")
 			Config.enemy:addParam("active", "Enabled", SCRIPT_PARAM_ONOFF, true)
 
 		Config:addParam("updateScript", "Update Script (rev. " .. latest .. ")", SCRIPT_PARAM_ONOFF, false)
-		Config.updateScript = false -- Prevent scriptConfig from reading saved value
-
-	-- Load the sprites
-	for _, spellName in pairs({"itemsmiteaoe", "s5_summonersmiteduel", "s5_summonersmiteplayerganker", "s5_summonersmitequick", 
-		"snowballfollowupcast", "summonerbarrier", "summonerboost", "summonerclairvoyance", "summonerdot", 
-		"summonerexhaust", "summonerflash", "summonerhaste", "summonerheal", "summonermana", 
-		"summonerodingarrison", "summonerpororecall", "summonerporothrow", "summonerrevive", "summonersmite", 
-		"summonersnowball", "summonerteleport"}) do
-		local location = "myVision\\spells\\" .. spellName .. ".png"
-
-		if FileExist(SPRITE_PATH .. location) then
-			summonerSprites[spellName] = createSprite(location)
-		else
-			Print(location .. " not found.")
-		end
-	end
-
-	summonerSprites["teleportcancel"] = summonerSprites["summonerteleport"]
-
-	for i = 1, heroManager.iCount do
-		local hero = heroManager:getHero(i)
-
-		if hero and hero.valid then
-			spellData[i] = {
-				[_Q] = hero:GetSpellData(_Q), 
-				[_W] = hero:GetSpellData(_W), 
-				[_E] = hero:GetSpellData(_E), 
-				[_R] = hero:GetSpellData(_R), 
-				[SUMMONER_1] = hero:GetSpellData(SUMMONER_1), 
-				[SUMMONER_2] = hero:GetSpellData(SUMMONER_2), 
-			}
-		end
-	end
+		Config.updateScript = false
 
 	Print("<font color=\"#77FF77\">Loaded</font>")
+end
+
+function OnUnload()
+	if abilityFrame then
+		abilityFrame:Release()
+	end
+
+	for spellName, sprite in pairs(summonerSprites) do
+		if sprite then
+			sprite:Release()
+		end
+	end
 end
 
 function OnWndMsg(a, b)
@@ -79,62 +93,76 @@ function OnWndMsg(a, b)
 end
 
 function OnDraw()
-	for i = 1, heroManager.iCount do
-		local hero = heroManager:getHero(i)
-
-		if hero and hero.visible and not hero.dead and hero.bTargetable and ((hero.isMe and Config.ally.self) or (hero.team == TEAM_ENEMY and Config.enemy.active) or (Config.ally.active and not hero.isMe)) then
-			local screenPos = WorldToScreen(D3DXVECTOR3(hero.x, hero.y, hero.z))
+	for _, ally in pairs(allyHeroes) do
+		if Config.ally.active and ally.visible and ally.bTargetable and not ally.dead then
+			local framePos = GetAbilityFramePos(ally)
 				
-			if OnScreen(screenPos, screenPos) then
-				-- Draw the hero's abilities
-				local framePos = GetAbilityFramePos(hero)
-
-				-- Draw the ability frame
-				if abilityFrame then
-					abilityFrame:Draw(framePos.x, framePos.y, 255)
-				end
-
-				for spellID = _Q, _R do
-					local spellData = spellData[i][spellID]
-
-					if spellData.level > 0 then
-						local spellPos = Point(framePos.x + 3 + (spellID * 26), framePos.y + 3)
-
-						if spellData.currentCd < 1 and not spellData.isToggleSpell then -- Ability is off cooldown
-							DrawLine(spellPos.x, spellPos.y, spellPos.x + 25, spellPos.y, 3, ARGB(255, 50, 205, 50))
-						elseif spellData.isToggleSpell and spellData.toggleState > 0 then
-							DrawLine(spellPos.x, spellPos.y, spellPos.x + 25, spellPos.y, 3, spellData.toggleState == 2 and ARGB(255, 50, 205, 50) or ARGB(255, 46, 139, 87))
-						else
-							DrawLine(spellPos.x, spellPos.y, spellPos.x + 25, spellPos.y, 3, ARGB(255, 128, 0, 0))
-
-							DrawLine(spellPos.x, spellPos.y, spellPos.x + (spellData.totalCooldown - spellData.currentCd) / spellData.totalCooldown * 25, spellPos.y, 3, ARGB(255, 46, 139, 87))
-
-							local strCooldown = tostring(round(spellData.currentCd))
-							DrawText(strCooldown, 14, spellPos.x - GetTextArea(strCooldown, 14).x / 2 + 12.5, spellPos.y + 5, ARGB(255, 255, 255, 255))
-						end
-					end
-				end
-
-				-- Draw the hero's summoner spells
-				local barPos = hero.isMe and Point(framePos.x + 110, framePos.y - 16) or Point(framePos.x - 13, framePos.y - 16)
-					
-				for spellID = SUMMONER_1, SUMMONER_2 do
-					local spellData = spellData[i][spellID]
-					local midPos = Point(barPos.x + 6, barPos.y + (spellID - SUMMONER_1) * 13 + 6)
-
-					if spellData.currentCd > 0 then
-						local angle = math.rad(-90 + (spellData.totalCooldown - spellData.currentCd) / spellData.totalCooldown * 360)
-
-						if summonerSprites[spellData.name] then
-							summonerSprites[spellData.name]:Draw(barPos.x, barPos.y + (spellID - SUMMONER_1) * 13, 128)
-						end
-						--DrawLine(midPos.x, midPos.y, midPos.x + math.cos(angle - 0.1) * 6, midPos.y + math.sin(angle - 0.1) * 6, 1, ARGB(255, 0, 0, 0)) -- Shadow
-						DrawLine(midPos.x, midPos.y, midPos.x + math.cos(angle) * 6, midPos.y + math.sin(angle) * 6, 1, ARGB(255, 255, 255, 255))
-					elseif summonerSprites[spellData.name] then
-						summonerSprites[spellData.name]:Draw(barPos.x, barPos.y + (spellID - SUMMONER_1) * 13, 255)
-					end
-				end
+			if OnScreen(framePos, framePos) then
+				DrawOverheadHUD(ally, framePos)
 			end
+		end
+	end
+
+	for _, enemy in pairs(enemyHeroes) do
+		if Config.enemy.active and enemy.visible and enemy.bTargetable and not enemy.dead then
+			local framePos = GetAbilityFramePos(enemy)
+				
+			if OnScreen(framePos, framePos) then
+				DrawOverheadHUD(enemy, framePos)
+			end
+		end
+	end
+end
+
+function DrawOverheadHUD(unit, framePos)
+	if abilityFrame then
+		abilityFrame:Draw(framePos.x, framePos.y, 255)
+	else
+		DrawRectangle(framePos.x, framePos.y - 1, 110, 9, 0xFF000000)
+		DrawRectangleOutline(framePos.x, framePos.y - 1, 110, 9, 0xFF737173, 1)
+		DrawRectangleOutline(framePos.x + 1, framePos.y, 108, 7, 0xFF4A494A, 1)
+	end
+
+	for spellID = _Q, _R do
+		local pSpellData = spellData[unit.index][spellID]
+
+		if pSpellData.level > 0 then
+			local framePos = Point(framePos.x + 3, framePos.y + 3)
+			local spellPos = Point(framePos.x + (spellID * 26), framePos.y)
+
+			if pSpellData.isToggleSpell and pSpellData.toggleState > 0 then
+				DrawLine(spellPos.x, spellPos.y, spellPos.x + 25, spellPos.y, 3, pSpellData.toggleState == 2 and 0xFF32CD32 or 0xFF2E8B57)
+			elseif pSpellData.currentCd <= 0 then
+				DrawLine(spellPos.x, spellPos.y, spellPos.x + 25, spellPos.y, 3, 0xFF32CD32)
+			else
+				DrawLine(spellPos.x, spellPos.y, spellPos.x + 25, spellPos.y, 3, 0xFF800000)
+				if pSpellData.totalCooldown > 0 then
+					DrawLine(spellPos.x, spellPos.y, spellPos.x + (pSpellData.totalCooldown - pSpellData.currentCd) / pSpellData.totalCooldown * 25, spellPos.y, 3, 0xFF2E8B57)
+				end
+				local strCooldown = tostring(pSpellData.currentCd < 10 and math.round(pSpellData.currentCd, 1) or math.ceil(pSpellData.currentCd))
+				DrawText(strCooldown, 14, spellPos.x - GetTextArea(strCooldown, 14).x / 2 + 12.5, spellPos.y + 5, 0xFFFFFFFF)
+			end
+
+			DrawLine(spellPos.x, spellPos.y + 1, spellPos.x + 25, spellPos.y + 1, 1, 0x3F000000)
+		end
+	end
+
+	local barPos = Point(framePos.x - 13, framePos.y - 18)
+					
+	for spellID = SUMMONER_1, SUMMONER_2 do
+		local pSpellData = spellData[unit.index][spellID]
+		local pSprite = summonerSprites[pSpellData.name]
+		local midPos = Point(barPos.x + 6, barPos.y + (spellID - SUMMONER_1) * 13 + 6)
+
+		if pSpellData.currentCd > 0 then
+			local angle = rad(-90 + (pSpellData.totalCooldown - pSpellData.currentCd) / pSpellData.totalCooldown * 360)
+
+			if pSprite then
+				pSprite:Draw(barPos.x, barPos.y + (spellID - SUMMONER_1) * 13, 128)
+			end
+			DrawLine(midPos.x, midPos.y, midPos.x + cos(angle) * 6, midPos.y + sin(angle) * 6, 1, 0xFFFFFFFF)
+		elseif pSprite then
+			pSprite:Draw(barPos.x, barPos.y + (spellID - SUMMONER_1) * 13, 255)
 		end
 	end
 end
@@ -143,33 +171,25 @@ end
 	Utility functions
 
 	Print(value) - Prints value to chat.
-	round(float) - Returns the rounded number from float.
 	GetAbilityFramePos(unit) - Returns the top-left corner of where the ability frame should be positioned.
 ]]
 
 function Print(value)
-	PrintChat("<font color=\"#AAAAAA\"><b>myVision (rev. " .. revision .. ")</b>: </font><font color=\"#FFFFFF\">" .. value .. "</font>")
-end
-
-function round(a)
-	local b = math.floor(a)
-	if a >= b + 0.5 then
-		return math.ceil(a)
-	else
-		return math.floor(a)
-	end
+	PrintChat("<font color=\"#AAAAAA\"><b>myVision (rev. " .. REVISION .. ")</b>: </font><font color=\"#FFFFFF\">" .. value .. "</font>")
 end
 
 function GetAbilityFramePos(unit)
-	if unit.isMe then
-		local barPos = GetUnitHPBarPos(unit)
-		local barOffset = GetUnitHPBarOffset(unit)
-		return Point(barPos.x - 45, barPos.y + barOffset.y * 50 + 9)
-	else
-		local barPos = GetUnitHPBarPos(unit)
-		local barOffset = GetUnitHPBarOffset(unit)
-		return Point(barPos.x - 69, barPos.y + barOffset.y * 50 + 12)
+	local barPos = GetUnitHPBarPos(unit)
+	local barOffset = GetUnitHPBarOffset(unit)
+
+	do -- For some reason the x offset never exists
+		local t = {
+			["Renekton"] = -0.05
+		}
+		barOffset.x = t[unit.charName] or 0
 	end
+
+	return Point(barPos.x - 69 + barOffset.x * 150, barPos.y + barOffset.y * 52.5 + 12.5)
 end
 
 -- ScriptStatus Lua Plugin
